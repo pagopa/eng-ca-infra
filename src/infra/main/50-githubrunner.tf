f4eb2f98b441734cc6d0f453500d076d636bc6f9
Walter Traspadini <walter.traspadini@pagopa.it>
2023-10-25 16:13:50 +0200

    fix: Update vault config action (#16)
    * update vault config action
    
    * update action
    
    * fix cluster name
    
    * fix wrong container name
    
    * update task def image and missed loggroup
    
    custom runner image
    
    custom runner image

--- src/infra/main/50-githubrunner.tf
+++ src/infra/main/50-githubrunner.tf
@@ -1,5 +1,25 @@
+resource "aws_cloudwatch_log_group" "ecs_github_runner" {
+  name = "github/runners"
+
+  retention_in_days = var.ecs_logs_retention_days
+
+  tags = {
+    Name = "vault"
+  }
+}
+
+# TODO: review
+resource "aws_ecr_repository" "runner_ecr" {
+  name                 = "github-runner"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
 resource "aws_ecs_task_definition" "github_runner_def" {
-  count                    = 2
   family                   = format("%s-githubrunner", local.project)
   execution_role_arn       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ecsTaskExecutionRole"
   task_role_arn            = aws_iam_role.ecs_vault_task_role.arn
@@ -11,11 +31,11 @@ resource "aws_ecs_task_definition" "github_runner_def" {
 [
   {
     "name": "githubrunner",
-    "image": "ghcr.io/actions/actions-runner:2.311.0",
+    "image": "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/github-runner:1",
     "logConfiguration": {
       "logDriver": "awslogs",
       "options": {
-        "awslogs-group": "github/runners",
+        "awslogs-group": "${aws_cloudwatch_log_group.ecs_github_runner.id}",
         "awslogs-region": "${var.aws_region}",
         "awslogs-stream-prefix": "run"
       }
