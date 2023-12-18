7b9a5743a1b8449f85f6eb56595b889768aca72c
Alessandro Sebastiani <sebbalex@gmail.com>
2023-12-18 13:24:23 +0100

    fix: api address in terraform

--- src/infra/vault/00.tf
+++ src/infra/vault/00.tf
@@ -12,8 +12,8 @@ resource "vault_mount" "root" {
 
 resource "vault_pki_secret_backend_config_urls" "root" {
   backend                 = vault_mount.root.path
-  issuing_certificates    = ["https://${var.app_primary_domain_name}/00/ca"]
-  crl_distribution_points = ["https://${var.app_primary_domain_name}/00/crl"]
+  issuing_certificates    = ["https://${var.api_primary_domain_name}/00/ca"]
+  crl_distribution_points = ["https://${var.api_primary_domain_name}/00/crl"]
 }
 
 resource "vault_pki_secret_backend_crl_config" "root" {
